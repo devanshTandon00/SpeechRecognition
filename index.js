af3613fc55719108c749a8407a8fd4d0d5f60611
devanshTandon00 <dtdansh@gmail.com>
2020-07-15 14:42:13 +0530

    Commit

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const searchBar = document.querySelector('#searchBar');
-const mic = searchBar.querySelector('i.fa.fa-microphone');
+const mic = document.querySelector('i.fa.fa-microphone');
+const textBox = document.querySelector('textBox');
+const micIcon = document.querySelector('i');
 
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 window.SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
 const recognition = new SpeechRecognition();
+
+// results are continuous
 recognition.continuous = true;
+
+// results are shown on screen side by side as user says things
 recognition.interimResults = true;
+
+const msg = () => {
+  mic.addEventListener('click', () => {
+    // the classList will output fa at position 0 and fa-microphone at position 1.
+    if(micIcon.classList.contains("fa-microphone")){
+      micIcon.classList.remove("fa-microphone");
+      micIcon.classList.add("fa-microphone-slash");
+      recognition.start();
+      console.log("Mic is clicked");
+    }
+    else{
+
+    }
+  });
+}
+
 
 if(recognition){
   console.log("Your Browser supports speech Recognition");
+  msg();
 }
 
-mic.addEventListener('click', () => {
-  recognition.start();
-});
+// mic.
 
 // recognition.addEventListener('start', function() {
 //   console.log('Speech recognition service has started');
